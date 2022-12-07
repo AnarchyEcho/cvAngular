8c25eb5694b3f7eaa093a1c0e3ed490435de5bb5
kodeandre <andre.kodehode@gmail.com>
2022-12-07 16:36:27 +0100

    Data fetched and displayed with consistent styling

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../Project';
 import { ProjectsService } from '../projects.service';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -9,6 +10,7 @@ import { ProjectsService } from '../projects.service';
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [];
+  faGithub = faGithub;
 
   getProjects() {
     this.projectsService.getProjects()
